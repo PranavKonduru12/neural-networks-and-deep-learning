f5a48067da70f3829ceef014d59e92a6b047986f
Pranav Konduru <pskonduru12+gh@gmail.com>
2022-05-05 01:05:30 -0700

    Fixed syntax in SGD and updated range command

--- src/network.py
+++ src/network.py
@@ -53,18 +53,18 @@ class Network(object):
         tracking progress, but slows things down substantially."""
         if test_data: n_test = len(test_data)
         n = len(training_data)
-        for j in xrange(epochs):
+        for j in range(epochs):       # update xrange with range in python 3
             random.shuffle(training_data)
             mini_batches = [
                 training_data[k:k+mini_batch_size]
-                for k in xrange(0, n, mini_batch_size)]
+                for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print "Epoch {0}: {1} / {2}".format(
+                print ("Epoch {0}: {1} / {2}").format(
                     j, self.evaluate(test_data), n_test)
             else:
-                print "Epoch {0} complete".format(j)
+                print ("Epoch {0} complete").format(j)
 
     def update_mini_batch(self, mini_batch, eta):
         """Update the network's weights and biases by applying
